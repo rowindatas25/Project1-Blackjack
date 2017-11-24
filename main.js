3ea57d6655520d05f0dd045368e5d73672af1b68
Rohtas Wadera <rohtaswadera@Rohtass-Air.home>
2017-11-24 11:41:10 -0500

    random card created with start button

--- main.js
+++ main.js
@@ -3,15 +3,39 @@
 
 const cards = [];
 const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
-const numbers = ["A", "1", "2", "3", "4", "5", "6", "7", "8", 
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", 
 "9", "10", "J", "Q", "K"];
-const display = document.getElementById("display");
-const randomNum = Math.floor((Math.random()*52) + 1);
 
 
-
+// Assigning values and colors to suits with use of a for .. in loop
 for (s in suits) {
-    for (n in numbers) {
-        console.log(suits[s] + numbers[n]);
+    const suit = suits[s][0].toUpperCase();
+    const backgroundColor = (suit == "S" || suit == "C") ? "black" : "red"
+    for (n in numbers) { 
+   const value = (n > 9) ? 10 : parseInt(n) + 1 
+   const card = {
+        suit: suit,
+        icon: suits[s],
+        backgroundColor: backgroundColor,
+        cardNumber: numbers[n],
+        value: value
+    }
+    cards.push(card);
+
     }
 }
+
+console.log(cards);
+
+//  Creating random card function which creates a random card with the styling and color for each card is in the function
+
+function randomCard() {
+    
+
+const randomNum = Math.floor((Math.random()*52));
+display.innerHTML += "<span style= 'color: " + " " + cards[randomNum].backgroundColor + "'>" + " " + cards[randomNum].icon + " " + 
+cards[randomNum].cardNumber + "</span>  ";
+
+}
+
+randomCard();
